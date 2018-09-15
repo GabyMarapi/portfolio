4452c1d925431b90781e762b62fc42f9247dcd58
GabyMarapi <gaby1017x@gmail.com>
2018-09-15 00:02:38 -0500

    Efecto scroll navbar

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
 $(document).ready(()=>{
     $(".button-collapse").sideNav()
 }) 
+
+$(document).scroll(()=>{
+    if($(window).scrollTop()>300){
+        $('nav').addClass('navClass')
+    }
+    else{
+        $('nav').removeClass('navClass')        
+    }
+})
+$('.scrollspy').scrollSpy({
+    scrollOffset:80
+})
